646a696ad83387fc9f8655b2b184f1266b6f338e
Sanishka Jayasena (Home) <sanishka94@gmail.com>
2020-09-07 00:53:46 +0700

    laravel

---
+++ php/Laravel_Framework/learnOne/resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fbkk20-1.fna.fbcdn.net/v/t51.2885-19/s320x320/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fbkk20-1.fna.fbcdn.net&_nc_ohc=Vbn_gMPlSEAAX-KfrHe&oh=1dce1d3b13ed05b9ef02878f1fb67fbd&oe=5F7F261F" class="rounded-circle" style="width: 100%;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fbkk20-1.fna.fbcdn.net/v/t51.2885-15/e35/c95.0.560.560a/s320x320/118614410_230664505046765_5604987870734821551_n.jpg?_nc_ht=instagram.fbkk20-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=7clGgEFsPLEAX-5Csd1&oh=ad713ac4bd6073dcb1dd1ad710a4c911&oe=5F7D8CCC" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fbkk20-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.112.929.929a/s480x480/118605679_1451236268600542_2050874606066190756_n.jpg?_nc_ht=instagram.fbkk20-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=dVHJeoUv2BgAX9e7pBk&oh=d0fc1bc3d1d7d54977df295fbd1ed38a&oe=5F7D190A" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fbkk20-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c3.0.744.744a/s640x640/117185734_730508550844355_4827964239716308767_n.jpg?_nc_ht=instagram.fbkk20-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=vazdRNZ7SacAX9X1H0t&oh=c6280faae2edaa25ce4e363f455c8ef3&oe=5F7E474E" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
