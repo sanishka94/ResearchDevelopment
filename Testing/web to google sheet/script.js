3f7e4e72d009e61a3e90da887371ca6d28b3c293
Sanishka Jayasena (Home) <sanishka94@gmail.com>
2020-10-15 22:59:06 +0700

    testing

--- Testing/web to google sheet/script.js
+++ Testing/web to google sheet/script.js
@@ -1,20 +1,16 @@
 $( document ).ready(function() {
     $("#send").on("click", function() {
         var data = {
-            "name" : "sani",
-            "email" : "test@test.com"
+            "name" : "sanihka",
+            "email" : "testing@test.com"
         }
 
         $.ajax({
-            // url : "https://docs.google.com/forms/d/e/1FAIpQLSfQygzAgJj_Fx0_dytWYBHpxH-YxYT8NMCaVbTC6nPL2dxXDw/formResponse?",
-            // url : "https://docs.google.com/spreadsheets/d/15XxTW1lq8x4jNFBS0EI5lQbMmGTxyVR1GDYHcl9ZDtY",
             url : "https://script.google.com/macros/s/AKfycbwLO4ApRj8hxh6SzJ4VAByCfg007NHNIrAK3YUbk0PuAvzCMZA/exec",
             data : data,
             type : "post",
             jsonp: "callback",
             dataType : "jsonp",
-            // crossDomain: true,
-            // Access-Control-Allow-Origin: *,
             success : function(e){
                 console.log("Data sent");
             },
